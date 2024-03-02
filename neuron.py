df16799ae543ebe7f9c58d6624bb3136219eacc6
Scikous <rozeusker@gmail.com>
2024-03-02 09:05:30 +0200

    Changed necessary variables to utilize CPU instead of GPU, Updated README

--- neuron.py
+++ neuron.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image, ImageOps
-import glob
 # from numba import jit, cuda
 # from numba.experimental import jitclass
 from timeit import default_timer as timer  
@@ -13,17 +12,16 @@ import concurrent.futures as cf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
-import cupy as cp
 
 class neural_network_operations():
 
     def relu(self, activatables):#the activation function
-        return cp.maximum(0, activatables)
+        return np.maximum(0, activatables)
 
     def weighted_sum_output(self, inputs, weights, bias):#single neuron output, sum of inputs*weights -> scalar
         if inputs.ndim == 1:#inputs are of type vector
             if weights.ndim == 1:#assuming one neuron per layer
-                output = cp.dot(weights, inputs)
+                output = np.dot(weights, inputs)
                 output += bias
             else:
                 raise ValueError("Weights must be of type vector")
@@ -33,33 +31,33 @@ class neural_network_operations():
 
     def weighted_sum_layer(self, inputs, weights, biases):#for multiple neurons in layer (could be hidden layer or output layuer)
         if weights.ndim == 2:#assuming multiple neurons per layer
-            outputs = cp.matmul(weights,inputs)+biases
+            outputs = np.matmul(weights,inputs)+biases
         else:
             raise ValueError("Weights must be 2D matrix")
         return outputs.reshape(len(biases), 1)
 
     
     def sigmoid(self, output_layer_activations):
-        sigmoid = cp.array([])
-        print(cp.exp(-output_layer_activations[0]))
+        sigmoid = np.array([])
+        print(np.exp(-output_layer_activations[0]))
         for activation in output_layer_activations:
             probability =  0.0#
             #sum_activation_probs = 0.0            
-            probability = cp.exp(-activation)/(1+cp.exp(-activation))**2
-            sigmoid = cp.append(sigmoid, probability)
+            probability = np.exp(-activation)/(1+np.exp(-activation))**2
+            sigmoid = np.append(sigmoid, probability)
         return sigmoid
 
     def feed_forward(self, inputs, num_hidden_layers:int, output_layer_size:int, weights=None, biases=None, weight_size=64):
         all_outputs = []
         all_activations = [inputs]
         if weights == None:
-            first_hidden_layer_weights = cp.random.uniform(0.0, cp.sqrt(2/len(inputs)), size=(weight_size, len(inputs)))#first hidden takes in the input features
-            weights = [cp.random.uniform(0.0, cp.sqrt(2/len(inputs)), size=(weight_size, weight_size)) for _ in range(num_hidden_layers-1)] #ignore first hidden layer weights
+            first_hidden_layer_weights = np.random.uniform(0.0, np.sqrt(2/len(inputs)), size=(weight_size, len(inputs)))#first hidden takes in the input features
+            weights = [np.random.uniform(0.0, np.sqrt(2/len(inputs)), size=(weight_size, weight_size)) for _ in range(num_hidden_layers-1)] #ignore first hidden layer weights
             weights.insert(0, first_hidden_layer_weights)
-            output_layer_weights = cp.random.uniform(0.0, cp.sqrt(2/len(inputs)), size=(output_layer_size, weight_size))
+            output_layer_weights = np.random.uniform(0.0, np.sqrt(2/len(inputs)), size=(output_layer_size, weight_size))
             weights.append(output_layer_weights)
-            biases = [cp.array([cp.random.uniform(0.0, cp.sqrt(2/len(inputs)), size=(weight_size))]).T for _ in range(num_hidden_layers)]
-            output_layer_biases = cp.array([cp.random.uniform(0.0, cp.sqrt(2/len(inputs)), size=(output_layer_size))]).T
+            biases = [np.array([np.random.uniform(0.0, np.sqrt(2/len(inputs)), size=(weight_size))]).T for _ in range(num_hidden_layers)]
+            output_layer_biases = np.array([np.random.uniform(0.0, np.sqrt(2/len(inputs)), size=(output_layer_size))]).T
             biases.append(output_layer_biases)
         ind = 0
 
@@ -104,9 +102,9 @@ class neural_network_operations():
         probabilities = output_layer
         epsilon = 1e-10
         if isinstance(expected_layer, (int, float)):
-            loss = -(expected_layer*cp.log(probabilities[0]+epsilon) +(1-expected_layer)*cp.log(1-probabilities[0]+epsilon))
+            loss = -(expected_layer*np.log(probabilities[0]+epsilon) +(1-expected_layer)*np.log(1-probabilities[0]+epsilon))
         else:
-            loss = -cp.sum((expected_layer*cp.log(probabilities+epsilon)))
+            loss = -np.sum((expected_layer*np.log(probabilities+epsilon)))
         return loss
     
     def cross_entropy_loss_backpropagation(self, output_layer, expected_layer):
@@ -116,19 +114,19 @@ class neural_network_operations():
         return loss
     
     def relu_backpropagation(self, activatables) -> 0 | 1:#derivative of relu
-        return (activatables > 0).astype(cp.int32)
+        return (activatables > 0).astype(np.int32)
     
     def softmax(self, output_layer_activations):
-        e_output = cp.exp(output_layer_activations - cp.max(output_layer_activations))
+        e_output = np.exp(output_layer_activations - np.max(output_layer_activations))
         return e_output / e_output.sum(axis=0)
 
     #redundant
     # def softmax_backpropagation(self, softmax):
     #         # Reshape the 1-d softmax to 2-d so that np.dot will do the matrix multiplication
     #     s = softmax.reshape(-1, 1)
-    #     print(s.shape, cp.diagflat(s).shape)
+    #     print(s.shape, np.diagflat(s).shape)
 
-    #     return cp.diagflat(s) - cp.dot(s, s.T)
+    #     return np.diagflat(s) - np.dot(s, s.T)
         #return self.softmax(activatables)*(1-self.softmax(activatables))
     
     def cross_entropy_softmax_derivative(self, output_probs, expected):
@@ -141,15 +139,15 @@ class neural_network_operations():
         if isinstance(cost_gradient, (int, float)):#if only one gradient value
             return cost_gradient*activations_backpropagation
         else:
-            hadamard_res = cp.multiply(cost_gradient, activations_backpropagation)
+            hadamard_res = np.multiply(cost_gradient, activations_backpropagation)
         return hadamard_res.reshape(len(activations_backpropagation), 1)
 
-    def output_layer_errors(self, output_layer_outputs: cp.array, output_layer_activations: cp.array, expected_layer: cp.array) -> cp.array:#gets the last layer's errors
+    def output_layer_errors(self, output_layer_outputs: np.array, output_layer_activations: np.array, expected_layer: np.array) -> np.array:#gets the last layer's errors
         errors = self.cross_entropy_softmax_derivative(output_layer_activations, expected_layer) #relu derivative
         return errors
     
     def layer_errors(self, previous_layer_weights, previous_layer_errors, current_layer_outputs):        
-        weighted_sum_errors = cp.matmul(previous_layer_weights.T, previous_layer_errors)
+        weighted_sum_errors = np.matmul(previous_layer_weights.T, previous_layer_errors)
         layer_errors = self.hadamard_product(weighted_sum_errors, self.relu_backpropagation(current_layer_outputs))
         return layer_errors
     
@@ -170,29 +168,29 @@ class neural_network_operations():
 
         # Vectorized computation of all_cost_wrt_weights
         all_activations.pop(-1)
-        all_cost_wrt_weights = [cp.matmul(layer_errors, prev_layer_activations.T) for layer_errors, prev_layer_activations in zip(all_cost_wrt_biases, all_activations[::-1])]
+        all_cost_wrt_weights = [np.matmul(layer_errors, prev_layer_activations.T) for layer_errors, prev_layer_activations in zip(all_cost_wrt_biases, all_activations[::-1])]
         return all_cost_wrt_weights[::-1], all_cost_wrt_biases[::-1]
 
 
 
     def batch_averages(self, batch_weights, batch_biases):#take the average of the weights and biases of the batch
-        mean_weights_first_hidden = cp.zeros_like(batch_weights[0][0])
-        mean_weights = [cp.zeros_like(batch_weights[0][1]) for _ in range(len(batch_weights[0])-2)]#first and output layer can have different neuron sizes
-        mean_output_weights = cp.zeros_like(batch_weights[0][-1])#output layer can have different num of neurons
+        mean_weights_first_hidden = np.zeros_like(batch_weights[0][0])
+        mean_weights = [np.zeros_like(batch_weights[0][1]) for _ in range(len(batch_weights[0])-2)]#first and output layer can have different neuron sizes
+        mean_output_weights = np.zeros_like(batch_weights[0][-1])#output layer can have different num of neurons
         mean_weights.append(mean_output_weights)
         mean_weights.insert(0, mean_weights_first_hidden)
-        mean_biases = [cp.zeros_like(batch_biases[0][0]) for _ in range(len(batch_biases[0])-1)]
-        mean_output_biases = cp.zeros_like(batch_biases[0][-1])#output layer can have different num of neurons
+        mean_biases = [np.zeros_like(batch_biases[0][0]) for _ in range(len(batch_biases[0])-1)]
+        mean_output_biases = np.zeros_like(batch_biases[0][-1])#output layer can have different num of neurons
         mean_biases.append(mean_output_biases)
         for weights_set, biases_set in zip(batch_weights, batch_biases):#each batch element's set of weights and biases
             for ind, (weights, biases) in enumerate(zip(weights_set, biases_set)):#for each layer sum weights with previous set of weights
-                cp.add(mean_weights[ind], weights, out=mean_weights[ind])#mean_weights[ind] += weights
-                cp.add(mean_biases[ind], biases, out=mean_biases[ind])#mean_biases[ind] += biases
+                np.add(mean_weights[ind], weights, out=mean_weights[ind])#mean_weights[ind] += weights
+                np.add(mean_biases[ind], biases, out=mean_biases[ind])#mean_biases[ind] += biases
 
         #after summation divide to get mean
         for ind in range(len(mean_biases)):
-            cp.divide(mean_weights[ind], len(batch_weights), out=mean_weights[ind])
-            cp.divide(mean_biases[ind], len(batch_biases), out=mean_biases[ind])
+            np.divide(mean_weights[ind], len(batch_weights), out=mean_weights[ind])
+            np.divide(mean_biases[ind], len(batch_biases), out=mean_biases[ind])
             #mean_weights[ind] /= len(batch_weights)
             #mean_biases[ind] /= len(batch_biases)
         return mean_weights, mean_biases
@@ -229,7 +227,7 @@ class neural_network_operations():
         total_avg_loss = 0.0
         print(num_classes, learning_rate)
         for epoch in range(1, epochs+1):#start at 1
-            average_epoch_loss = cp.array([])
+            average_epoch_loss = np.array([])
             epoch_weights, epoch_biases = [], []
 
             print(f"Epoch/Epochs: {epoch}/{epochs}")
@@ -239,10 +237,10 @@ class neural_network_operations():
             for batch_results in epoch_results:
                 epoch_weights.append(batch_results[0])
                 epoch_biases.append(batch_results[1])
-                average_epoch_loss = cp.append(average_epoch_loss, batch_results[2])
+                average_epoch_loss = np.append(average_epoch_loss, batch_results[2])
             final_weights, final_biases = self.batch_averages(epoch_weights, epoch_biases)
             
-            average_epoch_loss = cp.sum(average_epoch_loss)/num_batches
+            average_epoch_loss = np.sum(average_epoch_loss)/num_batches
             total_avg_loss += average_epoch_loss
             print(f"Epoch loss: {average_epoch_loss}, Average Epoch Loss: {total_avg_loss/epoch}")
             if average_epoch_loss < total_avg_loss/epochs or epoch == 1:
@@ -279,8 +277,8 @@ class neural_network_operations():
                 
         with open(model_file_path, 'r') as json_file:
             data = json.load(json_file)
-        weights = [cp.array(saved_weights) for saved_weights in data["weights"]]
-        biases = [cp.array(saved_biases) for saved_biases in data["biases"]]
+        weights = [np.array(saved_weights) for saved_weights in data["weights"]]
+        biases = [np.array(saved_biases) for saved_biases in data["biases"]]
         num_hidden_layers = data["hidden_layers"]
         classes = data["classes"]
 
@@ -293,7 +291,7 @@ class neural_network_operations():
 
         for test_img, test_label in zip(batch_test_imgs[0], batch_test_labels[0]):
             _, all_activations, _, _ = self.feed_forward(test_img, num_hidden_layers, num_classes, weights, biases)
-            if cp.argmax(all_activations[-1]) == cp.argmax(test_label):
+            if np.argmax(all_activations[-1]) == np.argmax(test_label):
                 num_correct += 1
         accuracy = num_correct/len(batch_test_imgs[0])
         print(f"Testing accuracy:  {accuracy} \nCorrect/Total: {num_correct}/{len(batch_test_imgs[0])}")
@@ -303,8 +301,8 @@ class neural_network_operations():
     def batch_processor(self, imgs, labels, batch_size):#only works with cupy arrays
         num_batches = self.get_num_batches(imgs, batch_size)
         print("Num Batches: ", num_batches)
-        indices = cp.arange(imgs.shape[0])
-        cp.random.shuffle(indices)
+        indices = np.arange(imgs.shape[0])
+        np.random.shuffle(indices)
         
         imgs_shuffled = imgs[indices]
         labels_shuffled = labels[indices]
@@ -322,15 +320,15 @@ def images_loader(dir_path, img_resize=35, num_imgs=0,grayscale=False, load_from
     try:
         if not load_from_file:
             raise Exception("Number of imgs changed, defaulting to loader")
-        image_list = cp.load(f'image_list_{dir_path}.npy')
-        ground_truths = cp.load(f'ground_truths_{dir_path}.npy')
+        image_list = np.load(f'image_list_{dir_path}.npy')
+        ground_truths = np.load(f'ground_truths_{dir_path}.npy')
         print("Found pre-saved images")
     except Exception as e:
         print(e)
         subdirectories = [d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
         num_classes= len(subdirectories)
-        ground_truths = cp.empty((0, num_classes))
-        image_list = cp.empty((0, img_resize, img_resize)) if grayscale else cp.empty((0,img_resize,img_resize,3))
+        ground_truths = np.empty((0, num_classes))
+        image_list = np.empty((0, img_resize, img_resize)) if grayscale else np.empty((0,img_resize,img_resize,3))
         print(subdirectories)
         for subdir in subdirectories:#loop through directories of classes and get labels and vectorized versin of all imgs
             imgs_path = os.path.join(dir_path, subdir)
@@ -342,21 +340,21 @@ def images_loader(dir_path, img_resize=35, num_imgs=0,grayscale=False, load_from
                             img = ImageOps.grayscale(Image.open(img_path).resize((img_resize, img_resize)))
                         else:
                             img = Image.open(img_path).resize((img_resize,img_resize))
-                        img_array = cp.asarray(img)
+                        img_array = np.asarray(img)
                         #code for removing non-RGB files
                         """if img_array.ndim != 3 or img_array.shape[2] != 3:
                             print(img_array.shape, img_path)
                             os.remove(img_path)
                             continue """
-                        image_list = cp.concatenate((image_list, img_array[cp.newaxis, :, :]), axis=0)
-                        label = cp.zeros( (1, num_classes) )# * len(subdirectories)
+                        image_list = np.concatenate((image_list, img_array[np.newaxis, :, :]), axis=0)
+                        label = np.zeros( (1, num_classes) )# * len(subdirectories)
                         label[0][subdirectories.index(subdir)] = 1
-                        ground_truths = cp.concatenate((ground_truths, label), axis=0)#multiple label for each image in class
+                        ground_truths = np.concatenate((ground_truths, label), axis=0)#multiple label for each image in class
                 else:
                     break
         if save_to_file:
-            cp.save(f'image_list_{dir_path}.npy', image_list)
-            cp.save(f'ground_truths_{dir_path}.npy', ground_truths)
+            np.save(f'image_list_{dir_path}.npy', image_list)
+            np.save(f'ground_truths_{dir_path}.npy', ground_truths)
     print(len(ground_truths), len(image_list))
     return image_list, ground_truths
 
@@ -368,8 +366,8 @@ def mnist_batches(train_imgs, train_labels, batch_size, batch_num):
     batch_imgs = train_imgs[batch_take_from:batch_take_until]
 
     #reshape to form nx1
-    batch_labels = cp.array([train_label.reshape(len(train_label),1) for train_label in train_labels[batch_take_from:batch_take_until]])
-    batch_imgs = cp.array([(img.reshape(-1,1)/255.0) for img in batch_imgs])
+    batch_labels = np.array([train_label.reshape(len(train_label),1) for train_label in train_labels[batch_take_from:batch_take_until]])
+    batch_imgs = np.array([(img.reshape(-1,1)/255.0) for img in batch_imgs])
     return batch_imgs, batch_labels
 
 
@@ -380,14 +378,14 @@ if __name__=="__main__":
     train_path = 'train'
     batch_size = 128
     
-    #train_imgs, train_labels = images_loader(train_path, num_imgs=60000, grayscale=False, load_from_file=True, save_to_file=False)
-    #neuron_ops.model_trainer(train_imgs[:50000], train_labels[:50000], batch_size, 12)
+    train_imgs, train_labels = images_loader(train_path, num_imgs=60000, grayscale=False, load_from_file=True, save_to_file=False)
+    neuron_ops.model_trainer(train_imgs[:50000], train_labels[:50000], batch_size, 12)
     test_imgs, test_labels = images_loader(test_path, num_imgs=9, grayscale=False, load_from_file=False, save_to_file=False)
 
     #MNIST handwritten digits dataset
     # (train_imgs,train_labels) , (test_imgs, test_labels) = mnist.load_data()
     # train_labels, test_labels = to_categorical(train_labels), to_categorical(test_labels)#for softmax classification
-    # train_imgs, train_labels, test_imgs, test_labels = cp.asarray(train_imgs), cp.asarray(train_labels), cp.asarray(test_imgs), cp.asarray(test_labels)
+    # train_imgs, train_labels, test_imgs, test_labels = np.asarray(train_imgs), np.asarray(train_labels), np.asarray(test_imgs), np.asarray(test_labels)
 
     model = "model_checkpoint.json"
     weights, biases, num_hidden_layers, classes = neuron_ops.model_loader(model)
@@ -404,5 +402,5 @@ if __name__=="__main__":
     preds_to_list = all_activations[-1].ravel().tolist()
     preds_to_list = [round(pred, 4) for pred in preds_to_list]
     print(f"Predicted values:  {dict(zip(classes, preds_to_list))} \nTrue values: {dict(zip(classes, batch_test_labels[0][ind].reshape(-1).ravel().tolist()))}")
-    plt.imshow(cp.asnumpy(test_img.reshape(reshape_to)), cmap='gray')
+    plt.imshow(np.asnumpy(test_img.reshape(reshape_to)), cmap='gray')
     plt.show()
